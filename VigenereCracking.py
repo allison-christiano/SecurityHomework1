862f039048222929d123fa198ab94cc3e6195e5a
allisonchristiano <chrial20@wfu.edu>
2024-02-08 23:56:50 -0500

    vigenere cracking complete

--- VigenereCracking.py
+++ VigenereCracking.py
@@ -57,32 +57,44 @@ def get_caesar_shift(encrypted_message1, expected_dist):
 
 
 def get_vigenere_keyword(enc_message, size, expected_dist):
+
+    best_correlation = -1
+    keyword1 = ""
+    vig_keyword = ""
     # provides calculations for the columnar transposition like process
     num = len(enc_message) // size
     encrypted_messages = [''] * size
     # partitions the message according to how it should be to enter the get_caesar_shift function
     for col in range(size):
         for row in range(num):
-            i = col + row * size
-            encrypted_messages[col] += enc_message[i]
+            j = col + row * size
+            encrypted_messages[col] += enc_message[j]
+
     # use the get_caesar_shift function on the messages to find the shift and convert the shift into a letter
-    vig_keyword = ""
-    for i in encrypted_messages:
-        shift = get_caesar_shift(i, expected_dist)
+    correlation = 0
+    for k in encrypted_messages:
+        shift = get_caesar_shift(k, expected_dist)
         character = chr(shift + 65)
         vig_keyword += character
-    return vig_keyword
+        correlation += shift
+
+        # update the keyword if correlation for this key size is higher
+        if correlation > best_correlation:
+            best_correlation = correlation
+            keyword1 = vig_keyword
+
+    return keyword1
 
 
 if __name__ == '__main__':
-    message = ("TEZHRAIRGMQHNJSQPTLNZJNEVMQHRXAVASLIWDNFOELOPFWGZUHSTIRGLUMCSW GTTQCSJULNLQK OHL MHCMPWLCEHTFNUHNPHTSFFADJHTLNBYORWEFRYE PIISO K ZQR GMPTLQCSPRMOCMKESMTYLUTFRMIEOWXXFMWECCLWSQGWUASSWFGTTMYSGU L QNQGEFGTTIDSWMOAGMKEOQL U KOVN AMZHZRGACMKZRHSQLKLBMJAXTKLVRGFCBTLNAM SMYAHEGIEHTKNFOELNBMWFGORHWTPAY MVOSGUVUSPD")
-    keyword_size = 4
+    message = (
+        "TEZHRAIRGMQHNJSQPTLNZJNEVMQHRXAVASLIWDNFOELOPFWGZUHSTIRGLUMCSW GTTQCSJULNLQK OHL MHCMPWLCEHTFNUHNPHTSFFADJHTLNBYORWEFRYE PIISO K ZQR GMPTLQCSPRMOCMKESMTYLUTFRMIEOWXXFMWECCLWSQGWUASSWFGTTMYSGU L QNQGEFGTTIDSWMOAGMKEOQL U KOVN AMZHZRGACMKZRHSQLKLBMJAXTKLVRGFCBTLNAM SMYAHEGIEHTKNFOELNBMWFGORHWTPAY MVOSGUVUSPD")
     freq_dist = {' ': .1828846265, 'E': .1026665037, 'T': .0751699827, 'A': .0653216702, 'O': .0615957725,
                  'N': .0571201113, 'I': .0566844326, 'S': .0531700534, 'R': .0498790855, 'H': .0497856396,
                  'L': .0331754796, 'D': .0328292310, 'U': .0227579536, 'C': .0223367596, 'M': .0202656783,
                  'F': .0198306716, 'W': .0170389377, 'G': .0162490441, 'P': .0150432428, 'Y': .0142766662,
                  'B': .0125888074, 'V': 0.0079611644, 'K': 0.0056096272, 'X': 0.0014092016, 'J': 0.0009752181,
                  'Q': 0.0008367550, 'Z': 0.0005128469}
-
-    keyword = get_vigenere_keyword(message, keyword_size, freq_dist)
+    for i in range(1, len(message)):
+        keyword = get_vigenere_keyword(message, i, freq_dist)
     print(keyword)
